1cdb9bb4b8af38b429c76b26a47a49f159ecf962
Taggart1993 <mylesemanship@gmail.com>
2022-03-04 07:13:02 -0800

    readying collectivenet, added fold

---
+++ collectivenet/collectivenet_start.bat
@@ -0,0 +1,3 @@
+title CollectiveNet Panel
+java -Xmx2G -Xms2G -jar forge-1.12.2-14.23.5.2860.jar --nogui
+pause 
--- collectivenet/start.bat
+++
@@ -1,3 +0,0 @@
-title The Collective Net
-java -Xmx2G -Xms2G -jar C:\Users\Myles\Desktop\TheCollectiveNet\jar\forge_1.12.2.jar --nogui
-pause
